c840de590724909d9f72471dd95c82ec695fdc1b
David Angell <davidjangell42@gmail.com>
2022-10-03 20:44:02 -0400

    ass2

--- Assignment 1/C1A1E1_main.cpp
+++ Assignment 1/C1A1E1_main.cpp
@@ -1,22 +1,35 @@
+//
+// David Angell - U09609277
+// davidjangell42@gmail.com
+// C/C++ Programming I : Fundamental Programming Concepts
+// 167109 - Raymond L. Mitchell
+// 10/3/2022
+// C1A1E1_main.cpp
+// Ubuntu 20.04
+// GCC 6.3.0
+//
+
 #include <iostream>
 
 int main() {
+    std::cout 
+        << "Enter two space-separated positive integer numeric values."
+        << "The first must be in hexadecimal while the second must be in octal."
+        << "Do not precede the values with 0x or 0\n";
+    
     int hexAsDecimal, octalAsDecimal;
-    std::cout << "Enter two space-separated positive integer numeric values. The first must be in hexadecimal while the second must be in octal.  Do not precede the values with 0x or 0" << std::endl;
-    std::cin >> std::hex >> hexAsDecimal;
-    std::cin >> std::oct >> octalAsDecimal;
-
+    std::cin 
+        >> std::hex >> hexAsDecimal
+        >> std::oct >> octalAsDecimal;
     
     std::cout 
-        << "   H " << std::hex << hexAsDecimal 
-        << " = O " << std::oct << hexAsDecimal 
-        << " = D " << std::dec << hexAsDecimal
-        << std::endl
-        
-        << "   H " << std::hex << octalAsDecimal 
-        << " = O " << std::oct << octalAsDecimal 
-        << " = D " << std::dec << octalAsDecimal
-        << std::endl;
+        << std::hex << hexAsDecimal << " hexadecimal = "
+        << std::oct << hexAsDecimal << " octal = "
+        << std::dec << hexAsDecimal << " decimal\n"
+
+        << std::oct << octalAsDecimal << " octal = "
+        << std::hex << octalAsDecimal << " hexadecimal = "
+        << std::dec << octalAsDecimal << " decimal\n";
 
     return 0;
 }
--- Assignment 1/C1A1E2_main.c
+++ Assignment 1/C1A1E2_main.c
@@ -1,11 +1,22 @@
+//
+// David Angell - U09609277
+// davidjangell42@gmail.com
+// C/C++ Programming I : Fundamental Programming Concepts
+// 167109 - Raymond L. Mitchell
+// 10/3/2022
+// C1A1E2_main.c
+// Ubuntu 20.04
+// GCC 6.3.0
+//
+
 #include <stdio.h>
 
-int main() {
+int main(void) {
     printf("Enter any number: ");
     
     double x;
     scanf("%lf", &x);
-    double xAfterEquation = 3*x*x*x - 5*x*x - 6;
+    double xAfterEquation = 3 * x * x * x - 5 * x * x + 6;
 
     printf("If x = %g the value of \"3x^3 - 5x^2 + 6\" is %g\n", x, xAfterEquation);
 
